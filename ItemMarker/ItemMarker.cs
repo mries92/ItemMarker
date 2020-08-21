8779d1c29149b62b3c2cc43b57828704df0e241a
Mike Ries <mries92@gmail.com>
2020-08-21 19:07:42 -0400

    Initial commit. Mod loads.

---
+++ ItemMarker/ItemMarker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx;
+using R2API;
+
+namespace ItemMarker
+{
+    [BepInPlugin("org.mries.itemmarker", "ItemMarker", "1.0.0")]
+    [BepInProcess("Risk of Rain 2.exe")]
+    public class ItemMarker : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Loaded ItemMarker");
+            On.RoR2.GenericPickupController.CreatePickup += CreatePickup;
+        }
+
+        private static RoR2.GenericPickupController CreatePickup(On.RoR2.GenericPickupController.orig_CreatePickup orig, ref global::RoR2.GenericPickupController.CreatePickupInfo createPickupInfo)
+        {
+            UnityEngine.Debug.Log("YOU PICKED UP AN ITEM");
+            return orig(ref createPickupInfo);
+        }
+    }
+}
